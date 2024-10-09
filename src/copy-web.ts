3651b3255c341813334972378cc3909ecd545eb6
Lehenna <lehenna@outlook.com>
2024-10-09 13:45:35 -0700

    fix copy script

--- src/copy-web.ts
+++ src/copy-web.ts
@@ -2,7 +2,7 @@ import { copy, remove, ensureDir } from "fs-extra";
 import { join } from "path";
 
 const WEB_PATH = join(process.cwd(), "web", "dist");
-const PUBLIC_PATH = join(process.cwd(), "build/public");
+const PUBLIC_PATH = join(process.cwd(), "build", "public");
 
 async function main() {
   await remove(PUBLIC_PATH);
